1524cb57d2966c6a8b6475aa6cdeb9c87e7d9ed9
madsvnielsen <mads@vnielsen.dk>
2019-09-26 13:37:52 +0200

    Removed data sets as they are too big for github. Added program

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# digitstat
-A look on Benford's Law
+# Digitstat
+A look on Benford's Law.
+I hope you don't program regularly, because this program is a mess. It is not optimized and was written during
+a boring lecture. Thanks for reading anyways :)
+
+# What is Benford's Law?
+
+From WikiPedia:
+Benford's law, also called the Newcomb–Benford law, the law of anomalous numbers, or the first-digit law, is an observation about the frequency distribution of leading digits in many real-life sets of numerical data. The law states that in many naturally occurring collections of numbers, the leading significant digit is likely to be small.[1] For example, in sets that obey the law, the number 1 appears as the leading significant digit about 30% of the time, while 9 appears as the leading significant digit less than 5% of the time. If the digits were distributed uniformly, they would each occur about 11.1% of the time. Benford's law also makes predictions about the distribution of second digits, third digits, digit combinations, and so on.
+
+# What this script does
+This Python script takes csv files in a subdirectory called "Data Sets" and spits out data, so you can see how well each particular data set fits on the Benford's Law.
+This script was written during a boring class, so please don't judge my lazy programming. 
+
+
--- digitstat.py
+++ digitstat.py
@@ -1,7 +1,87 @@
-'''
-DATA SETS:
-- Crimes in Boston
-- Mount Rainier Weather and Climbing Data
-- International football results from 1872 to 2019
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os import listdir
+from os.path import isfile, join
 
 '''
+I hope you don't program regularly, because this program is a mess. It is not optimized and was written during
+a boring lecture. Thanks for reading anyways :) 
+'''
+
+data_sets = [f for f in listdir("Data Sets") if isfile(join("Data Sets", f))] #Get CSV files in subdirectory "data sets"
+
+print("Analyzing %s data sets..." % len(data_sets))  #Write the amount of fetched data sets
+
+data_entry_count = 0   #Value to store number of data entries
+
+
+out = "" #The output to be written when all is analyzed
+data_index = 0  #An index used when iterating though the files. Used to determine the progress
+
+for csv in data_sets: #Iterate through all data set files
+    file = open("Data Sets/" + csv, "r", encoding='mac_roman') #Open the current csv file
+    data_entries = file.readlines() #Get an array with the lines (data_entries)
+    data_entry_count += len(data_entries) #Add this to the data_entry_count to keep track of data_entries to be analyzed
+    print("Data set %s/%s (%s)" % (data_index +1,len(data_sets), csv))  #Print which data set is curently being analyzed
+    entry_index = 0 #Index when iterating through the data entries to keep track of the progress
+    digit_count = 10 * [0] #Empty array, holding the statistics of the occurrence of each digit.
+
+    for data_entry in data_entries: #Iterate through all data entries
+
+        if round(entry_index/data_entry_count*100,3) % 10 == 0: #If the percentage of entries completed divided by 10 has 0 leftovers
+            print("%s percent done" % str(round(entry_index/data_entry_count*100,0))) #Print progress
+            print(digit_count)  #Print current statistics
+
+
+        significant_digit = True #Set that the next digit is significant
+        for char in data_entry: #Iterate through the data entries
+            if significant_digit: #If the digit is significant
+                #Following is a very stupid and slow way to determine if the digit is a number
+                if char is "0":
+                    digit_count[0] += 1
+                elif char is "1":
+                    digit_count[1] += 1
+                elif char is "2":
+                    digit_count[2] += 1
+                elif char is "3":
+                    digit_count[3] += 1
+                elif char is "4":
+                    digit_count[4] += 1
+                elif char is "5":
+                    digit_count[5] += 1
+                elif char is "6":
+                    digit_count[6] += 1
+                elif char is "7":
+                    digit_count[7] += 1
+                elif char is "8":
+                    digit_count[8] += 1
+                elif char is "9":
+                    digit_count[9] += 1
+
+                significant_digit = False #Next is to set that the next digit isn't significant
+
+
+            else: #Set that the next digit is significant if the current digit isn't a number
+                try:
+                    val = int(char)
+                except ValueError:
+                    if(char != "."):
+                        significant_digit = True
+        entry_index += 1
+    data_index += 1
+    significant_all = sum(digit_count) #Get amount of significant digits
+    digit_percent = 10 * [0]
+    for i in range(len(digit_count)): #Calculate the percentage (and remember to handle division by zero :p
+        try:
+            digit_percent[i] = round((digit_count[i]/significant_all)*100, 2)
+        except:
+            digit_percent[i] = 0
+
+    out += "File: %s\n" % csv #Write data to output string
+    out += "Digit counts: %s\n" % digit_count
+    out += "Digit percent: %s \n" % digit_percent
+
+#Print output :)
+print("Analyzed %s data entries" % data_entry_count)
+print(out)
+
